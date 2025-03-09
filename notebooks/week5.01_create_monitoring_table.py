368fcd3fc525604a9211a6a9efb46ef3b2ea94a8
ateya-ai <ateyatech.ai@gmail.com>
2025-03-09 15:55:43 -0500

    Completed monitoring files setup

--- notebooks/week5.01_create_monitoring_table.py
+++ notebooks/week5.01_create_monitoring_table.py
@@ -1,264 +1,245 @@
-# Databricks notebook source
-# !pip install wine_quality-1.1.3-py3-none-any.whl
+# # Databricks notebook source
+# # !pip install wine_quality-1.1.3-py3-none-any.whl
 
-# COMMAND ----------
+# # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Feature Importance
+# # MAGIC %md
+# # MAGIC ## Feature Importance
 
-# COMMAND ----------
+# # COMMAND ----------
 
-import pandas as pd
-from pyspark.sql.functions import col
-from pyspark.sql.functions import current_timestamp, to_utc_timestamp
-import numpy as np
-from pyspark.sql import SparkSession
+# import datetime
+# import itertools
+# import time
 
-from wine_quality.config import ProjectConfig
+# import pandas as pd
+# import requests
+# from databricks.connect import DatabricksSession
+# from databricks.sdk import WorkspaceClient
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import col, current_timestamp, to_utc_timestamp
+# from sklearn.preprocessing import LabelEncoder
 
+# from wine_quality.config import ProjectConfig
+# from wine_quality.data_processor import generate_synthetic_data
+# from wine_quality.monitoring import create_or_refresh_monitoring
 
-spark = SparkSession.builder.getOrCreate()
+# spark = SparkSession.builder.getOrCreate()
 
-# Load configuration
-config = ProjectConfig.from_yaml(config_path="project_config.yml", env="dev")
-spark = SparkSession.builder.getOrCreate()
+# # Load configuration
+# config = ProjectConfig.from_yaml(config_path="project_config.yml", env="dev")
+# spark = SparkSession.builder.getOrCreate()
 
-train_set = spark.table(f"{config.catalog_name}.{config.schema_name}.train_set").toPandas()
-test_set = spark.table(f"{config.catalog_name}.{config.schema_name}.test_set").toPandas()
+# train_set = spark.table(f"{config.catalog_name}.{config.schema_name}.train_set").toPandas()
+# test_set = spark.table(f"{config.catalog_name}.{config.schema_name}.test_set").toPandas()
 
-# COMMAND ----------
+# # COMMAND ----------
 
 
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
+# # Encode categorical and datetime variables
+# def preprocess_data(df):
+#     label_encoders = {}
+#     for col in df.select_dtypes(include=["object", "datetime"]).columns:
+#         le = LabelEncoder()
+#         df[col] = le.fit_transform(df[col].astype(str))
+#         label_encoders[col] = le
+#     return df, label_encoders
 
-# Encode categorical and datetime variables
-def preprocess_data(df):
-    label_encoders = {}
-    for col in df.select_dtypes(include=['object', 'datetime']).columns:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col].astype(str))
-        label_encoders[col] = le
-    return df, label_encoders
 
-train_set, label_encoders = preprocess_data(train_set)
+# train_set, label_encoders = preprocess_data(train_set)
 
-# Define features and target (adjust columns accordingly)
-features = train_set.drop(columns=["quality"])
-target = train_set["quality"]
+# # Define features and target (adjust columns accordingly)
+# features = train_set.drop(columns=["quality"])
+# target = train_set["quality"]
 
-# Train a Random Forest model
-model = RandomForestClassifer(random_state=42)
-model.fit(features, target)
+# # Train a Random Forest model
+# model = RandomForestClassifer(random_state=42)
+# model.fit(features, target)
 
-# Identify the most important features
-feature_importances = pd.DataFrame({
-    'Feature': features.columns,
-    'Importance': model.feature_importances_
-}).sort_values(by='Importance', ascending=False)
+# # Identify the most important features
+# feature_importances = pd.DataFrame({"Feature": features.columns, "Importance": model.feature_importances_}).sort_values(
+#     by="Importance", ascending=False
+# )
 
-print("Top 5 important features:")
-print(feature_importances.head(5))
+# print("Top 5 important features:")
+# print(feature_importances.head(5))
 
 
-# COMMAND ----------
+# # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ## Generate Synthetic Data
+# # MAGIC %md
+# # MAGIC ## Generate Synthetic Data
 
-# COMMAND ----------
-from wine_quality.data_processor import generate_synthetic_data
+# # COMMAND ----------
 
-inference_data_skewed = generate_synthetic_data(train_set, drift= True, num_rows=200)
 
-# COMMAND ----------
+# inference_data_skewed = generate_synthetic_data(train_set, drift=True, num_rows=200)
 
-# MAGIC %md
-# MAGIC ### Create Tables and Update house_features_online
+# # COMMAND ----------
 
-# COMMAND ----------
+# # MAGIC %md
+# # MAGIC ### Create Tables and Update house_features_online
 
-inference_data_skewed_spark = spark.createDataFrame(inference_data_skewed).withColumn(
-    "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
-)
+# # COMMAND ----------
 
-inference_data_skewed_spark.write.mode("overwrite").saveAsTable(
-    f"{config.catalog_name}.{config.schema_name}.inference_data_skewed"
-)
+# inference_data_skewed_spark = spark.createDataFrame(inference_data_skewed).withColumn(
+#     "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+# )
 
-# COMMAND ----------
+# inference_data_skewed_spark.write.mode("overwrite").saveAsTable(
+#     f"{config.catalog_name}.{config.schema_name}.inference_data_skewed"
+# )
 
-import time
-from databricks.sdk import WorkspaceClient
+# # COMMAND ----------
 
-workspace = WorkspaceClient()
 
-#write into feature table; update online table
-spark.sql(f"""
-    INSERT INTO {config.catalog_name}.{config.schema_name}.wine_quality_features
-    SELECT Id, fixed_acidity, citric_acid, volatile_acidity
-    FROM {config.catalog_name}.{config.schema_name}.inference_data_skewed
-""")
-  
-update_response = workspace.pipelines.start_update(
-    pipeline_id=config.pipeline_id, full_refresh=False)
-while True:
-    update_info = workspace.pipelines.get_update(pipeline_id=config.pipeline_id, 
-                            update_id=update_response.update_id)
-    state = update_info.update.state.value
-    if state == 'COMPLETED':
-        break
-    elif state in ['FAILED', 'CANCELED']:
-        raise SystemError("Online table failed to update.")
-    elif state == 'WAITING_FOR_RESOURCES':
-        print("Pipeline is waiting for resources.")
-    else:
-        print(f"Pipeline is in {state} state.")
-    time.sleep(30)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## Send Data to the Endpoint
-
-# COMMAND ----------
-
-import pandas as pd
-from pyspark.sql.functions import col
-from pyspark.sql.functions import current_timestamp, to_utc_timestamp
-import numpy as np
-import datetime
-import itertools
-from pyspark.sql import SparkSession
-
-from wine_quality.config import ProjectConfig
-
-spark = SparkSession.builder.getOrCreate()
-
-# Load configuration
-config = ProjectConfig.from_yaml(config_path="project_config.yml", env="prd")
-
-test_set = spark.table(f"{config.catalog_name}.{config.schema_name}.test_set") \
-                        .withColumn("Id", col("Id").cast("string")) \
-                        .toPandas()
-
-
-inference_data_skewed = spark.table(f"{config.catalog_name}.{config.schema_name}.inference_data_skewed") \
-                        .withColumn("Id", col("Id").cast("string")) \
-                        .toPandas()
-
-
-# COMMAND ----------
-
-token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
-host = spark.conf.get("spark.databricks.workspaceUrl")
-
-# COMMAND ----------
-
-
-from databricks.sdk import WorkspaceClient
-import requests
-import time
-
-workspace = WorkspaceClient()
-
-# Required columns for inference
-required_columns = [
-    "alcohol",
-    "density",
-    "volatile_acidity",
-    "chlorides",
-    "residual_sugar",
-    "free_sulfur_dioxide",
-    "pH",
-    "total_sulfur_dioxide",
-    "citric_acid",
-    "fixed_acidity"
-    "sulphates",
-    "type",
-    "Id",
-]
-
-# Sample records from inference datasets
-sampled_skewed_records = inference_data_skewed[required_columns].to_dict(orient="records")
-test_set_records = test_set[required_columns].to_dict(orient="records")
-
-# COMMAND ----------
-
-# Two different way to send request to the endpoint
-# 1. Using https endpoint
-def send_request_https(dataframe_record):
-    model_serving_endpoint = f"https://{host}/serving-endpoints/wine-quality-model-serving-fe-dev/invocations
-"
-    response = requests.post(
-        model_serving_endpoint,
-        headers={"Authorization": f"Bearer {token}"},
-        json={"dataframe_records": [dataframe_record]},
-    )
-    return response
-
-# 2. Using workspace client
-def send_request_workspace(dataframe_record):
-    response = workspace.serving_endpoints.query(
-        name="wine-quality-model-serving-fe-dev",
-        dataframe_records=[dataframe_record]
-    )
-    return response
-
-# COMMAND ----------
-
-# Loop over test records and send requests for 10 minutes
-end_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
-for index, record in enumerate(itertools.cycle(test_set_records)):
-    if datetime.datetime.now() >= end_time:
-        break
-    print(f"Sending request for test data, index {index}")
-    response = send_request_https(record)
-    print(f"Response status: {response.status_code}")
-    print(f"Response text: {response.text}")
-    time.sleep(0.2)
-
-
-# COMMAND ----------
-
-# Loop over skewed records and send requests for 10 minutes
-end_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
-for index, record in enumerate(itertools.cycle(sampled_skewed_records)):
-    if datetime.datetime.now() >= end_time:
-        break
-    print(f"Sending request for skewed data, index {index}")
-    response = send_request_https(record)
-    print(f"Response status: {response.status_code}")
-    print(f"Response text: {response.text}")
-    time.sleep(0.2)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## Refresh Monitoring
-
-# COMMAND ----------
-
-from pyspark.sql.functions import col
-from databricks.connect import DatabricksSession
-from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, ArrayType
-from databricks.sdk import WorkspaceClient
-
-from wine_quality.config import ProjectConfig
-from wine_quality.monitoring import create_or_refresh_monitoring
-
-spark = DatabricksSession.builder.getOrCreate()
-workspace = WorkspaceClient()
-
-# Load configuration
-config = ProjectConfig.from_yaml(config_path="project_config.yml", env="prd")
-
-create_or_refresh_monitoring(config=config, spark=spark, workspace=workspace)
+# workspace = WorkspaceClient()
+
+# # write into feature table; update online table
+# spark.sql(f"""
+#     INSERT INTO {config.catalog_name}.{config.schema_name}.wine_quality_features
+#     SELECT Id, fixed_acidity, citric_acid, volatile_acidity
+#     FROM {config.catalog_name}.{config.schema_name}.inference_data_skewed
+# """)
+
+# update_response = workspace.pipelines.start_update(pipeline_id=config.pipeline_id, full_refresh=False)
+# while True:
+#     update_info = workspace.pipelines.get_update(pipeline_id=config.pipeline_id, update_id=update_response.update_id)
+#     state = update_info.update.state.value
+#     if state == "COMPLETED":
+#         break
+#     elif state in ["FAILED", "CANCELED"]:
+#         raise SystemError("Online table failed to update.")
+#     elif state == "WAITING_FOR_RESOURCES":
+#         print("Pipeline is waiting for resources.")
+#     else:
+#         print(f"Pipeline is in {state} state.")
+#     time.sleep(30)
+
+# # COMMAND ----------
+
+# # MAGIC %md
+# # MAGIC ## Send Data to the Endpoint
+
+# # COMMAND ----------
+
+
+# spark = SparkSession.builder.getOrCreate()
+
+# # Load configuration
+# config = ProjectConfig.from_yaml(config_path="project_config.yml", env="prd")
+
+# test_set = (
+#     spark.table(f"{config.catalog_name}.{config.schema_name}.test_set")
+#     .withColumn("Id", col("Id").cast("string"))
+#     .toPandas()
+# )
+
+
+# inference_data_skewed = (
+#     spark.table(f"{config.catalog_name}.{config.schema_name}.inference_data_skewed")
+#     .withColumn("Id", col("Id").cast("string"))
+#     .toPandas()
+# )
+
+
+# # COMMAND ----------
+
+# token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+# host = spark.conf.get("spark.databricks.workspaceUrl")
+
+# # COMMAND ----------
+
+
+# workspace = WorkspaceClient()
+
+# # Required columns for inference
+# required_columns = [
+#     "alcohol",
+#     "density",
+#     "volatile_acidity",
+#     "chlorides",
+#     "residual_sugar",
+#     "free_sulfur_dioxide",
+#     "pH",
+#     "total_sulfur_dioxide",
+#     "citric_acid",
+#     "fixed_acidity" "sulphates",
+#     "type",
+#     "Id",
+# ]
+
+# # Sample records from inference datasets
+# sampled_skewed_records = inference_data_skewed[required_columns].to_dict(orient="records")
+# test_set_records = test_set[required_columns].to_dict(orient="records")
+
+# # COMMAND ----------
+
+
+# # Two different way to send request to the endpoint
+# # 1. Using https endpoint
+# def send_request_https(dataframe_record):
+#     model_serving_endpoint = f"https://{host}/serving-endpoints/wine-quality-model-serving-fe-dev/invocations"
+#     response = requests.post(
+#         model_serving_endpoint,
+#         headers={"Authorization": f"Bearer {token}"},
+#         json={"dataframe_records": [dataframe_record]},
+#     )
+#     return response
+
+
+# # 2. Using workspace client
+# def send_request_workspace(dataframe_record):
+#     response = workspace.serving_endpoints.query(
+#         name="wine-quality-model-serving-fe-dev", dataframe_records=[dataframe_record]
+#     )
+#     return response
+
+
+# # COMMAND ----------
+
+# # Loop over test records and send requests for 10 minutes
+# end_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
+# for index, record in enumerate(itertools.cycle(test_set_records)):
+#     if datetime.datetime.now() >= end_time:
+#         break
+#     print(f"Sending request for test data, index {index}")
+#     response = send_request_https(record)
+#     print(f"Response status: {response.status_code}")
+#     print(f"Response text: {response.text}")
+#     time.sleep(0.2)
+
+
+# # COMMAND ----------
+
+# # Loop over skewed records and send requests for 10 minutes
+# end_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
+# for index, record in enumerate(itertools.cycle(sampled_skewed_records)):
+#     if datetime.datetime.now() >= end_time:
+#         break
+#     print(f"Sending request for skewed data, index {index}")
+#     response = send_request_https(record)
+#     print(f"Response status: {response.status_code}")
+#     print(f"Response text: {response.text}")
+#     time.sleep(0.2)
+
+# # COMMAND ----------
+
+# # MAGIC %md
+# # MAGIC
+
+# # COMMAND ----------
+
+# # MAGIC %md
+# # MAGIC ## Refresh Monitoring
+
+# # COMMAND ----------
+
+
+# spark = DatabricksSession.builder.getOrCreate()
+# workspace = WorkspaceClient()
+
+# # Load configuration
+# config = ProjectConfig.from_yaml(config_path="project_config.yml", env="prd")
+
+# create_or_refresh_monitoring(config=config, spark=spark, workspace=workspace)
--- notebooks/week5.02_create_alert.py
+++ notebooks/week5.02_create_alert.py
@@ -15,28 +15,31 @@ srcs = w.data_sources.list()
 
 
 alert_query = """
-SELECT 
+SELECT
   (COUNT(CASE WHEN wasserstein_distance > 0.1 THEN 1 END) * 100.0 / COUNT(CASE WHEN wasserstein_distance IS NOT NULL AND NOT isnan(wasserstein_distance) THEN 1 END)) AS percentage_higher_than_70000
 FROM ws_mlops.wine_schema.model_monitoring_drift_metrics"""
 
 
-query = w.queries.create(query=sql.CreateQueryRequestQuery(display_name=f'wine-quality-alert-query-{time.time_ns()}',
-                                                           warehouse_id=srcs[0].warehouse_id,
-                                                           description="Alert on wine quality model for Wasserstein Distance (data drift)",
-                                                           query_text=alert_query))
+query = w.queries.create(
+    query=sql.CreateQueryRequestQuery(
+        display_name=f"wine-quality-alert-query-{time.time_ns()}",
+        warehouse_id=srcs[0].warehouse_id,
+        description="Alert on wine quality model for Wasserstein Distance (data drift)",
+        query_text=alert_query,
+    )
+)
 
 alert = w.alerts.create(
-    alert=sql.CreateAlertRequestAlert(condition=sql.AlertCondition(operand=sql.AlertConditionOperand(
-        column=sql.AlertOperandColumn(name="features_shifted_over_pt1")),
+    alert=sql.CreateAlertRequestAlert(
+        condition=sql.AlertCondition(
+            operand=sql.AlertConditionOperand(column=sql.AlertOperandColumn(name="features_shifted_over_pt1")),
             op=sql.AlertOperator.GREATER_THAN_OR_EQUAL,
-            threshold=sql.AlertConditionThreshold(
-                value=sql.AlertOperandValue(
-                    double_value=80))),
-            display_name=f'wine-quality-wasserstein-alert-{time.time_ns()}',
-            query_id=query.id
-        )
+            threshold=sql.AlertConditionThreshold(value=sql.AlertOperandValue(double_value=80)),
+        ),
+        display_name=f"wine-quality-wasserstein-alert-{time.time_ns()}",
+        query_id=query.id,
     )
-
+)
 
 
 # COMMAND ----------
